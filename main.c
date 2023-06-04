5bb46f3bfaf0f0159da4443ecb08ec2ad4224e68
Tronic <113149052+tronicStart@users.noreply.github.com>
2023-06-04 11:53:42 -0500

    Inicio de la funcion get_GET();
    Inicio de la creacion de la funcion get_GET.

--- main.c
+++ main.c
@@ -34,25 +34,28 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 
 int main(){
     
-    //pagìnas
+    //pagìnas para hacer poder enviar las los archivos html correpondientes
     char *page_2 = "http://localhost:8090/servicios.html";
     char *main_page = "http://localhost:8090/index.html";
 
     //otros
     int result;
-
+    int result_get;
+    
+    //configuracion del servidor
     servidor.port = 8090;
     servidor.buffer_size = 4024; //No necesario
-    start_server_file(&servidor);
-
+    start_server_file(&servidor);//funcion para iniciar el servidor
+    //bucle principal para escuchar las solicitudes
     while (WEB_TRUE){
-        listen_server_file(&servidor, "index.html");
-        split_lines(get_response());
-        if (get_POST_GET(get_response(), "user=", 5, 6) != WEB_ERROR){
-            _post = get_POST_GET(get_response(), "user=", 5, 6);
+        listen_server_file(&servidor, "index.html");//funcion para enviar index.html como respuesta
+        split_lines(get_response());//funcion para mostrar la respuesta. . .
+        result_get = get_GET (get_response(), "img.png", 0, 7, &servidor);
+        if (get_POST(get_response(), "user=", 5, 6) != WEB_ERROR){
+            _post = get_POST(get_response(), "user=", 5, 6);
             search_database(1, "webci_test.db", "SELECT * FROM usuarios;", callback);
         }
-        result = save_response(get_response());
+        result = save_response(get_response());//funcion para guardar la respuestas enviadas en un archivo llamado: data_response.txt
         if (result == ERROR){
             listen_server_file(&servidor, "bad_address.html");
         }
--- webci.h
+++ webci.h
@@ -14,7 +14,7 @@
 #define V 200
 #define Q 50
 #define ERROR -1
-#define OK 0
+#define OK 2
 #define WEB_TRUE 1
 #define WEB_FALSE 0
 #define WEB_OK 1
@@ -25,40 +25,16 @@
 #define _GET char * get
 #define GET get
 
-struct data {
-    
-    char data[V];
-    int condicion;
-    char File_name[V];
-    
-};
-
 struct Server {
     
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
+    int server_fd, new_socket, valread, client_socket;
+    struct sockaddr_in address, client_address;
+    socklen_t client_address_length;
     int opt;
     int addrlen;
     int error;
     int port;
     int buffer_size;
-    
-};
-
-struct base_lite {
-    
-    char name_base[Q];
-    char sql[Q];
-    char insert_sql[Q];
-    
-};
-
-typedef struct sqlite3_insert {
-    
-    char * name;
-    char * table;
-    char * insert;
-    char select_query[V];
     
 };
 
@@ -82,10 +58,8 @@ int search_database(int start, const char *name, const *query, int (*callback)()
     
     if (start != 0){
         sqlite3 *db;
-
         char *error_msg = 0;
         int rc = sqlite3_open(name, &db);
-
         if (rc != SQLITE_OK){
             fprintf(stderr, "No se puede abrir la base de datos: %s\n", sqlite3_errmsg(db));
             return ERROR;
@@ -115,7 +89,7 @@ int save_response (const char * data){
     return WEB_OK;
 }
 
-char * get_POST_GET(const char* text, const char* word, int ini,int end_index) {
+char * get_POST(const char* text, const char* word, int ini,int end_index) {
     
     char* text_copy = strdup(text);
     char* line = strtok(text_copy, "\n");
@@ -138,6 +112,49 @@ char * get_POST_GET(const char* text, const char* word, int ini,int end_index) {
     return WEB_ERROR;
 }
 
+void client_sockets (struct Server * server){
+    
+    server->client_address_length = sizeof(server->client_address);
+    server->client_socket = accept(server->server_fd, (struct sockaddr*)&server->client_address, &server->client_address_length);
+    
+}
+
+char * get_GET (const char* text, const char* word, int ini,int end_index, struct Server * server) {
+    
+    char* text_copy = strdup(text);
+    char* line = strtok(text_copy, "\n");
+    char response[BUFFER_SIZE];
+    size_t bytesRead;
+    FILE * fp;
+
+    while (line != NULL) {
+        char* word_position = strstr(line, word);
+        if (word_position != NULL) {
+            size_t start_index = word_position + ini - line;
+            if (start_index + end_index <= strlen(line)) {
+                char* result = malloc(end_index + 1);
+                strncpy(result, line + start_index, end_index);
+                result[end_index] = '\0';
+                free(text_copy);
+                fp = fopen(result,"r");
+                
+                if(fp == NULL){
+                    return WEB_ERROR;
+                }
+                while ((bytesRead = fread(response, 1, sizeof(response), fp)) > 0) {
+                       send(server->client_socket, response, bytesRead, 0);
+                }
+                fclose(fp);
+                close(server->client_socket);
+                return WEB_OK;
+            }
+        }
+        line = strtok(NULL, "\n");
+    }
+    free(text_copy);
+    return WEB_ERROR;
+}
+
 void split_lines(const char *text) {
     
     char *text_copy = strdup(text);
@@ -151,6 +168,7 @@ void split_lines(const char *text) {
 }
 
 int search_words(const char *text, const char *word) {
+    
     char *text_copy = strdup(text);
     char *line = strtok(text_copy, "\n");
 
@@ -161,33 +179,28 @@ int search_words(const char *text, const char *word) {
         line = strtok(NULL, "\n");
     }
     free(text_copy);
-    return NULL;
+    return WEB_ERROR;
 }
 
 int send_email(smtp_email *email){
     
     CURL *curl;
     CURLcode res = CURLE_OK;
-
     curl = curl_easy_init();
+    
     if (curl){
         
-        curl_easy_setopt(curl, CURLOPT_URL, email->smtp_url);        // Configura el servidor SMTP
-        curl_easy_setopt(curl, CURLOPT_MAIL_FROM, email->mail_from); // Dirección de correo del remitente
-
+        curl_easy_setopt(curl, CURLOPT_URL, email->smtp_url);
+        curl_easy_setopt(curl, CURLOPT_MAIL_FROM, email->mail_from);
         struct curl_slist *recipients = NULL;
-        recipients = curl_slist_append(recipients, email->recipient); // Añade destinatarios
+        recipients = curl_slist_append(recipients, email->recipient);
         curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
-
-        curl_easy_setopt(curl, CURLOPT_USERNAME, email->user_smtp);     // Usuario SMTP
-        curl_easy_setopt(curl, CURLOPT_PASSWORD, email->password_smtp); // Contraseña SMTP
-
+        curl_easy_setopt(curl, CURLOPT_USERNAME, email->user_smtp);
+        curl_easy_setopt(curl, CURLOPT_PASSWORD, email->password_smtp);
         curl_easy_setopt(curl, CURLOPT_READFUNCTION, NULL);
         curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-
         curl_easy_setopt(curl, CURLOPT_READDATA, email->payload_text);
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-
         res = curl_easy_perform(curl);
 
         if (res != CURLE_OK)
@@ -200,21 +213,7 @@ int send_email(smtp_email *email){
 }
 
 char * get_response (){
-    
     return buffer;
-    
-}
-
-char * get_response_2 (){
-    
-    int i,j;
-    
-    for(i = 0; i < BUFFER_SIZE; i++){
-        for(j = 0; j < BUFFER_SIZE; j++){
-            buffer_2[i][j] = buffer;
-        }
-    }
-    
 }
 
 void server_file_response(int client_socket, const char *content_type, const char *content) {
@@ -234,7 +233,6 @@ int start_server_file (struct Server * server){
         perror("[Server] ");
         return ERROR;
     }
-    
     if (setsockopt(server->server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &server->opt, sizeof(server->opt))) {
         perror("[Server] ");
         return ERROR;
@@ -253,7 +251,6 @@ int start_server_file (struct Server * server){
         perror("[Server] ");
         return ERROR;
     }
-    
 }
 
 int listen_server_file (struct Server * server, const char * archivo){
@@ -270,7 +267,7 @@ int listen_server_file (struct Server * server, const char * archivo){
             perror("[Server] ");
             return ERROR;
         }
-
+        
         char html_content[server->buffer_size];
         size_t html_size = fread(html_content, sizeof(char), server->buffer_size, file);
         fclose(file);
@@ -281,57 +278,24 @@ int listen_server_file (struct Server * server, const char * archivo){
 }
 
 int buscaPalabra(const char* cadena, const char* palabra) {
+    
     int contador = 0;
-    char* copia = strdup(cadena);  // Copia la cadena de texto original
-
-    char* token = strtok(copia, " ");  // Divide la cadena en palabras utilizando el espacio como delimitador
+    char* copia = strdup(cadena);
+    char* token = strtok(copia, " ");
 
     while (token != NULL) {
-        if (strcmp(token, palabra) == 0) {  // Compara la palabra actual con la palabra buscada
+        if (strcmp(token, palabra) == 0) {
             contador++;
         }
-        token = strtok(NULL, " ");  // Obtiene el siguiente token
+        token = strtok(NULL, " ");
     }
 
-    free(copia);  // Libera la memoria asignada a la copia de la cadena
+    free(copia);
     
     if(contador < 0){
         return ERROR;
     }
     return contador;
-}
-
-int crear_sqlite_data (struct base_lite * base){
-    
-    sqlite3 *db;
-    char *err_msg = 0;
-    int rc;
-    
-    rc = sqlite3_open(base->name_base, &db);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "No se puede abrir la base de datos: %s\n", sqlite3_errmsg(db));
-        return 1;
-    }
-    
-    rc = sqlite3_exec(db, base->sql, 0, 0, &err_msg);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Error al crear la %s: %s\n", base->sql, err_msg);
-        sqlite3_free(err_msg);
-        return 1;
-    }
-    
-    char insert_sql[100];
-    sprintf(insert_sql, base->insert_sql);
-    
-    rc = sqlite3_exec(db, insert_sql, 0, 0, &err_msg);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Error al insertar datos: %s\n", err_msg);
-        sqlite3_free(err_msg);
-        return 1;
-    }
-    
-    sqlite3_close(db);
-    
 }
 
 void ini_server (struct Server * S){
@@ -346,13 +310,11 @@ int config_server (struct Server * S){
     
     S->error = ERROR;
     
-    // Crear el socket
     if ((S->server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("[Server] ");
         return S->error;
     }
     
-    // Configurar opciones del socket
     if (setsockopt(S->server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &S->opt, sizeof(S->opt))) {
         perror("[Server] ");
         return S->error;
@@ -362,7 +324,6 @@ int config_server (struct Server * S){
     S->address.sin_addr.s_addr = INADDR_ANY;
     S->address.sin_port = htons(S->port);
     
-    // Asociar el socket a la dirección y puerto
     if (bind(S->server_fd, (struct sockaddr *)&S->address, sizeof(S->address)) < 0) {
         perror("[Server] ");
         return S->error;
@@ -370,108 +331,22 @@ int config_server (struct Server * S){
 }
 
 int listen_server (struct Server * S, const char * response){
-    // Escuchar por conexiones entrantes
+    
     if (listen(S->server_fd, 3) < 0) {
         perror("[Server] ");
         return S->error;
     }
     
-    // Aceptar conexiones entrantes y manejarlas
         if ((S->new_socket = accept(S->server_fd, (struct sockaddr *)&S->address, (socklen_t*)&S->addrlen)) < 0) {
             perror("[Server] ");
             return S->error;
         }
-
-        // Leer la solicitud HTTP entrante
+        
         S->valread = read(S->new_socket, buffer, BUFFER_SIZE);
-        // Enviar la respuesta HTTP al cliente
         write(S->new_socket, response, strlen(response));
         printf("[Server]: Respuesta enviada\n");
 
         close(S->new_socket);
-    
-}
-
-void send_new_page (struct Server * S, const char * response){
-    
-    listen_server(&S,response);
-    
-}
-
-int _server_html_ (const char * response, int PORT, struct data * Data){
-    
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    
-    // Crear el socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("[Server] ");
-        exit(EXIT_FAILURE);
-    }
-
-    // Configurar opciones del socket
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("[Server] ");
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Asociar el socket a la dirección y puerto
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("[Server] ");
-        exit(EXIT_FAILURE);
-    }
-
-    // Escuchar por conexiones entrantes
-    if (listen(server_fd, 3) < 0) {
-        perror("[Server] ");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("[Server]: Servidor html iniciado. Esperando conexiones...\nEnlace: http://localhost:%i\n",PORT);
-
-    // Aceptar conexiones entrantes y manejarlas
-    while(Data->condicion) {
-        
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("[Server] ");
-            exit(EXIT_FAILURE);
-        }
-
-        // Leer la solicitud HTTP entrante
-        valread = read(new_socket, buffer, BUFFER_SIZE);
-        printf("[Server]: Solicitud recibida:\n%s\n", buffer);
-        
-        strcpy(Data->data,buffer);
-
-        // Enviar la respuesta HTTP al cliente
-        write(new_socket, response, strlen(response));
-        printf("[Server]: Respuesta enviada\n");
-
-        // Cerrar la conexión
-        close(new_socket);
-    }
-    return 0;
-}
-
-int save_data_buffer_server (struct data * Data){
-    
-    FILE * dat = fopen(Data->File_name,"a");
-    
-    if(dat == NULL){
-        return -1;
-    }
-    
-    fprintf(dat,"%s\n",Data->data);
-    fclose(dat);
-    
-    return 0;
     
 }
 
